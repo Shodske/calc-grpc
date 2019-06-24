e4afb4cd8569fabb21be0a819cdb436c8cab9790
Shady Khattab <shadmanx@gmail.com>
2019-06-24 14:14:57 +0200

    ✨ Created simple cli client for add endpoint

--- cmd/calc_client/main.go
+++ cmd/calc_client/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/Shodske/calc-grpc/pkg/calculator"
+	"google.golang.org/grpc"
+)
 
 func main() {
-	fmt.Print("gRPC calculator client stub")
+	// Read env variables for host and port, default to localhost:50051
+	host := os.Getenv("HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "50051"
+	}
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(host+":"+port, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := calculator.NewCalculatorClient(conn)
+
+	// Contact the server and print out its response.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+
+	args := os.Args[1:]
+	if len(args) != 3 {
+		log.Fatal("Incorrect number of arguments, usage: add x y")
+	}
+
+	var res *calculator.Result
+	x, err := strconv.ParseFloat(args[1], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+	y, err := strconv.ParseFloat(args[2], 64)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch args[0] {
+	case "add":
+		res, err = c.Add(ctx, &calculator.Values{X: x, Y: y})
+	case "sum":
+		fallthrough
+	case "evaluate":
+		log.Fatalf("Unimplemented command: %s", args[0])
+	default:
+		log.Fatalf("Unknown command: %s", args[0])
+	}
+
+	if err != nil {
+		log.Fatalf("Error while sending gRPC request: %v", err)
+	}
+	log.Printf("Result: %f", res.Value)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,11 @@ go 1.12
 
 require (
 	github.com/golang/protobuf v1.3.1
-	golang.org/x/net v0.0.0-20190603091049-60506f45cf65
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
+	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f // indirect
+	golang.org/x/sys v0.0.0-20190621203818-d432491b9138 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20190620144150-6af8c5fc6601 // indirect
 	google.golang.org/grpc v1.21.1
 )
